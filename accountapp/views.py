12e7ed452d30d7d34e8821683e473fd0ab946ec0
sera0911 <sim11241124@gmail.com>
2021-07-06 02:04:26 +0900

    django start

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 
 #시범삼아 헬로우월드만 나오게 함수 설정
 def hello_world(request):
-    return HttpResponse('Hello World!')  #alt+enter를 누르면 import를 선택가능
+    return HttpResponse('안녕하세요!')  #alt+enter를 누르면 import를 선택가능
