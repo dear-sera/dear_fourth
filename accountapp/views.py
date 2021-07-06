2fb6ade8b75767cd4159c397025ee7e6580c42a6
sera0911 <sim11241124@gmail.com>
2021-07-06 13:03:20 +0900

    django courser

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,4 +7,4 @@ from django.shortcuts import render
 
 #시범삼아 헬로우월드만 나오게 함수 설정
 def hello_world(request):
-    return render(request, 'base.html')  #render를 통해 연결을 원한다면 처음인자는 request로 요청, 두번째는 해당 html의 이름
+    return render(request, 'accountapp/hello_world.html')  #render를 통해 연결을 원한다면 처음인자는 request로 요청, 두번째는 해당 html의 이름
