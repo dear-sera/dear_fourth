e288554e255390c2e1871d888449e8a23238eae8
sera0911 <sim11241124@gmail.com>
2021-07-06 18:31:43 +0900

    django model 연동

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)  #null=공백에 대해서 없어도 되는지 = 안되니까 false
--- dear_fourth/settings.py
+++ dear_fourth/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'dear_fourth.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  #여기에 db가 있다는 것
     }
 }
 
